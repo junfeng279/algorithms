7d9d351cd6c66f974d43d4c425b8b2e459111517
junfeng279 <junfeng279@163.com>
2020-08-17 23:37:48 +0800

    数据结构与算法之美学习示例

---
+++ src/test/java/com/summer/algorithms/algorithms/AlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summer.algorithms.algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
