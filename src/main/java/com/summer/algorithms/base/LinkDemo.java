560afc174e243c1e60a464ea09b5ef94c6a0aac1
junfeng279 <junfeng279@163.com>
2020-08-21 23:02:16 +0800

    链表操作

--- src/main/java/com/summer/algorithms/base/LinkDemo.java
+++ src/main/java/com/summer/algorithms/base/LinkDemo.java
@@ -194,6 +194,101 @@ public class LinkDemo {
         head = preNode;
     }
 
+    /**
+     * 寻找单链表的中间节点：
+     * 方法一： 先求出链表的长度， 再遍历1/2链表长度， 寻找出链表的中间节点
+     * 方法二：
+     * 用两个指针遍历链表， 一个快指针， 一个慢指针
+     * 快指针每次向前移动2个节点， 慢指针一次向前移动一个节点，
+     * 当快指针移动到链表末尾， 慢指针所在的位置即为中间节点所在的位置
+     * @return 中间节点
+     */
+    public Node findMiddleNode(){
+        Node slowPoint = head;
+        Node quickPoint = head;
+        while(quickPoint.next != null && quickPoint.next.next != null){
+            slowPoint = slowPoint.next;
+            quickPoint = quickPoint.next.next;
+        }
+        return slowPoint;
+    }
+
+    /**
+     * 判断链表是否有环
+     * 设置快指针和慢指针， 慢指针每次走一步， 快指针每次走两步
+     * 当快指针与慢指针相等时， 就说明该链表有环
+     * @return 是否有环
+     */
+    public boolean isRinged(){
+        if(head == null){
+            return false;
+        }
+        Node slow = head;
+        Node quick = head;
+        while(quick.next!=null && quick.next.next!=null){
+            slow = slow.next;
+            quick = quick.next.next;
+            if(slow == quick){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 返回链表的最后一个节点
+     * @return 最后一个节点
+     */
+    public Node getLastNode(){
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    /**
+     * 在不知道头节点的情况下删除指定节点
+     * 删除按节点的重点在于找出其前节点， 使其前节点的指针指向后节点， 即跳过删除的节点
+     * 1 如果待删除的节点是尾结点， 由于单链表不知道其前节点， 没有办法删除
+     * 2 如果删除的节点不是尾结点
+     * @param n 需要删除的节点信息
+     * @return 是否删除成功
+     */
+    public boolean deleteSpecialNode(Node n){
+        if(n.next == null){
+            return false;
+        }else{
+            //交换节点和其后续节点中的数据
+            int temp = n.value;
+            n.value = n.next.value;
+            n.next.value = temp;
+            //删除后续节点
+            n.next = n.next.next;
+            return true;
+        }
+    }
+
+    /**
+     * 判断两个链表是否相交
+     * 两个链表相交， 则他们的尾结点一定相同， 比较两个链表的尾结点是否相同即可
+     * @param head1 链表1
+     * @param head2 链表2
+     * @return 是否相交
+     */
+    public boolean isCross(Node head1, Node head2){
+        Node temp1 = head1;
+        Node temp2 = head2;
+        while(temp1.next != null){
+            temp1 = temp1.next;
+        }
+        while (temp2.next != null){
+            temp2 = temp2.next;
+        }
+        return temp1 == temp2;
+    }
+
+
     private static class Node{
         int value;
         /**
