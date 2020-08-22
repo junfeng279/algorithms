1928b5f0399d84369fcab009f012e8f850fba547
junfeng279 <junfeng279@163.com>
2020-08-22 23:57:47 +0800

    插入排序

---
+++ src/main/java/com/summer/algorithms/base/sort/InsertSortDemo.java
@@ -0,0 +1,20 @@
+package com.summer.algorithms.base.sort;
+
+/**
+ * 插入排序
+ * @author adminstor
+ */
+public class InsertSortDemo {
+    public static void main(String[] args) {
+        int[] a = {3, 5, 4, 1, 2, 6};
+    }
+
+    public static void insertSort(int[] a, int n){
+        if(n<=1){
+            return;
+        }
+        for(int i=1; i<n; i++){
+            int value = a[i];
+        }
+    }
+}
